68f327fec717a7cc0849824a87dd5bfe718932b5
10991983 <10991983@NBLV17100011.adira.co.id>
2023-11-01 15:29:38 +0700

    upload git - 2

---
+++ Test Suites/RunTestListener.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>RunTestListener</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>b20e01c2-ebcf-4ff4-9b3f-aa89fa484ed6</testSuiteGuid>
+   <testCaseLink>
+      <guid>ebf26492-edc0-4dc0-a162-06df3f5f20ed</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/latihanCustomKeywordWeek6</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
